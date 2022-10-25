9ec30e2600083cb33b985f1b2267bf9caecdb1d8
Visual Student <visualstudent@zschie.edu.pl>
2022-10-25 16:00:06 +0200

    wyszukiwanie

--- biblioteka/wypozycz.xaml.cs
+++ biblioteka/wypozycz.xaml.cs
@@ -23,5 +23,10 @@ namespace biblioteka
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
